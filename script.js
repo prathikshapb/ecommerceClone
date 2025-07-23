5b997f8a47c378bce0b01cf8f07688070ccfd424
prathikshapb <prathikshapb@gmail.com>
2025-07-23 12:19:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetchProducts();
 });
+let cart = [];
+
+function addToCart(product) {
+  cart.push(product);
+  alert(`${product.title} added to cart!`);
+  updateCartCount();
+}
+
+function updateCartCount() {
+  const cartIcon = document.getElementById("cart-count");
+  if (cartIcon) {
+    cartIcon.innerText = cart.length;
+  }
+}
+
+// Update your product HTML rendering to include an Add to Cart button
+function createProductHTML(product) {
+  return `
+    <div class="box">
+      <div class="box-content">
+        <h2>${product.title}</h2>
+        <div class="box-img" style="background-image: url('${product.image}');"></div>
+        <p>Price: ₹${product.price}</p>
+        <button onclick='addToCart(${JSON.stringify(product)})'>Add to Cart</button>
+      </div>
+    </div>
+  `;
+}
